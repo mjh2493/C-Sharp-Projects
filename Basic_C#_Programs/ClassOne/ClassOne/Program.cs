383bbe670d7dac98ccc8cfb5da5b31d54b3bbf33
mjh2493 <mjh2493@gmail.com>
2022-01-28 16:05:43 -0500

    updates to classOne

--- Basic_C#_Programs/ClassOne/ClassOne/Program.cs
+++ Basic_C#_Programs/ClassOne/ClassOne/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClassOne
 {
+    // defined class as static
     static class Program
     {
         static void Main(string[] args)
@@ -15,28 +16,30 @@ namespace ClassOne
 
             double num1;
 
+            // calls on the function to get the output 
             math.getValues(out num1);
 
+            // prints the answer to the equation
             Console.WriteLine("Your number divided by two is: " + num1);
             Console.ReadLine();
         }
     }
     class Math
     {
+        // function that divides integers and gives back an int
         public void getValues(out int value)
         {
             Console.WriteLine("Please enter a whole number: ");
             int num1 = Convert.ToInt32(Console.ReadLine());
             value = num1 / 2;
         }
+        // method that divides double and gives back a double
         public void getValues(out double value)
         {
             Console.WriteLine("Please enter a decimal: ");
             double num1 = Convert.ToDouble(Console.ReadLine());
-            int num2 = Convert.ToInt32(num1);
-            int answer = num2 / 2;
-            double finalAnswer = Convert.ToDouble(answer);
-            value = finalAnswer;
+            double answer = num1 / 2;
+            value = answer;
         }
     }
 }
